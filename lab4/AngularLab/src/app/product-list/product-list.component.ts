e3015fe634cf23e0eae38886bb1e8cabd478ab5a
anuaradilbek <adilbek.anuar@gmail.com>
2024-02-18 22:18:20 +0600

    lab4

---
+++ lab4/AngularLab/src/app/product-list/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+import { Product } from '../products';
+
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+
+})
+export class ProductListComponent {
+
+  products = [...products];
+
+  share(products: Product) {
+    const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(products.link)}`;
+    window.open(telegramUrl, '_blank');
+  }
+  
+
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
